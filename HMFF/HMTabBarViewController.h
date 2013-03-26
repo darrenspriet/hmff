e35a8735b8f064e85d366ea7423b66955bb23bf7
Darren Spriet <darrenspriet@MohawkColleges-iMac.local>
2013-03-26 17:02:55 -0400

    Added app Logo and 2x Added HMFF image Added Ticket Button Added a Tab Bar controller class and set the delegate

---
+++ HMFF/HMTabBarViewController.h
@@ -0,0 +1,15 @@
+//
+//  HMTabBarViewController.h
+//  HMFF
+//
+//  Created by Darren Spriet on 2013-03-26.
+//  Copyright (c) 2013 HMFF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMTabBarViewController : UITabBarController <UITabBarControllerDelegate>
+
+
+
+@end
---
+++ HMFF/HMTabBarViewController.m
@@ -0,0 +1,62 @@
+//
+//  HMTabBarViewController.m
+//  HMFF
+//
+//  Created by Darren Spriet on 2013-03-26.
+//  Copyright (c) 2013 HMFF. All rights reserved.
+//
+
+#import "HMTabBarViewController.h"
+
+@interface HMTabBarViewController ()
+
+@end
+
+@implementation HMTabBarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setDelegate:self];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController{
+   
+    if (viewController == [self.viewControllers objectAtIndex:0]){
+        NSLog(@"Line Up Tab");
+    }
+    else if (viewController == [self.viewControllers objectAtIndex:1]){
+        NSLog(@"News Feed Tab");
+
+    }
+    else if (viewController == [self.viewControllers objectAtIndex:2]){
+        NSLog(@"Twitter Tab");
+        
+    }
+    else if (viewController == [self.viewControllers objectAtIndex:3]){
+        NSLog(@"Social Tab");
+        
+    }
+    else {
+        NSLog(@"More Tab");
+        
+    }
+}
+
+@end
