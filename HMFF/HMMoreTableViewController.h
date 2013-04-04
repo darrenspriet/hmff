045e814cec611c7fe5abc4c32faa0db7536a70ea
Darren Spriet <darrenspriet@MohawkColleges-iMac.local>
2013-04-04 17:04:27 -0400

    Got the Websites working although they are zoomed in so they must be zoomed all the way out

---
+++ HMFF/HMMoreTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  HMMoreTableViewController.h
+//  HMFF
+//
+//  Created by Darren Spriet on 2013-04-04.
+//  Copyright (c) 2013 HMFF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HMMoreWebBrowserViewController.h"
+
+
+@interface HMMoreTableViewController : UITableViewController
+
+@end
---
+++ HMFF/HMMoreTableViewController.m
@@ -0,0 +1,140 @@
+//
+//  HMMoreTableViewController.m
+//  HMFF
+//
+//  Created by Darren Spriet on 2013-04-04.
+//  Copyright (c) 2013 HMFF. All rights reserved.
+//
+
+#import "HMMoreTableViewController.h"
+
+@interface HMMoreTableViewController ()
+
+@end
+
+@implementation HMMoreTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Configure the cell...
+    if (indexPath.row==0) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"submitABand"];
+        return cell;
+    }
+    else if(indexPath.row==1) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"submitAVideo"];
+        return cell;
+    }
+    else{
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"goToTheWebsite"];
+        return cell;
+        
+    }
+
+}
+
+#pragma Prepare for Segue
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    HMMoreWebBrowserViewController *webBrowser = segue.destinationViewController;
+    //When the View is loaded it this container sets the delegats
+    if ([segue.identifier isEqualToString:@"submitABand"]){
+        webBrowser.passedURL=@"http://www.hmff.com/?page_id=403";
+    }
+    else if([segue.identifier isEqualToString:@"submitAVideo"]){
+        webBrowser.passedURL=@"http://www.hmff.com/?page_id=407";
+
+    }
+    else if([segue.identifier isEqualToString:@"goToTheWebsite"]){
+        webBrowser.passedURL=@"http://www.hmff.com";
+
+    }
+    
+      }
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+//#pragma mark - Table view delegate
+//
+//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//
+//}
+
+@end
