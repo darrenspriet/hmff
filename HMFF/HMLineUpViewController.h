9c9cec07dea03d0f2fe78ccc38ec773af786ccae
Darren Spriet <darrenspriet@gmail.com>
2013-04-12 21:47:01 -0400

    Working Schedule Storyboard now going to try and merge

--- HMFF/HMLineUpViewController.h
+++ HMFF/HMLineUpViewController.h
@@ -9,13 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface HMLineUpViewController : UIViewController
-//<UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource>
-//@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-//@property (weak, nonatomic) IBOutlet UITableView *tableViewOne;
-//@property (weak, nonatomic) IBOutlet UITableView *tableViewTwo;
-//@property(strong, nonatomic)NSString *passed;
-//
-//
-//@property (nonatomic, strong) NSArray *tableOneArray;
-//@property (nonatomic, strong) NSArray *tableTwoArray;
+
 @end
