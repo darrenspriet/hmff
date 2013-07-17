908f825415b493dfb25395f605e809a303113e50
Darren Spriet <darrenspriet@gmail.com>
2013-07-17 03:31:03 -0400

    Got the parsing working for the Submit pages Also displaying them the way i want Corrected a few issues with how they look

---
+++ HMFF/HMDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMDetailsViewController.h
+//  HMFF
+//
+//  Created by Darren Spriet on 2013-07-16.
+//  Copyright (c) 2013 HMFF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMDetailsViewController : UIViewController
+
+@end
---
+++ HMFF/HMDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  HMDetailsViewController.m
+//  HMFF
+//
+//  Created by Darren Spriet on 2013-07-16.
+//  Copyright (c) 2013 HMFF. All rights reserved.
+//
+
+#import "HMDetailsViewController.h"
+
+@interface HMDetailsViewController ()
+
+@end
+
+@implementation HMDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
